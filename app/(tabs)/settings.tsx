f84b1ed333d362a47e4aa55b017531c5f4ee1b8b
Fernando Bergem Mayandia <fernandomayandia@icloud.com>
2025-09-07 19:58:49 +0200

    Added light sphere

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,3 +1,4 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
@@ -5,6 +6,14 @@ export default function SettingsScreen() {
   return (
     <SafeAreaView style={s.safe}>
       <View style={s.screen}>
+        <LinearGradient
+          colors={["rgba(120,180,255,0.35)", "rgba(255,255,255,0)"]}
+          start={{ x: 0.1, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          style={s.glow}
+          pointerEvents="none"
+/>
+
         <Text style={{color : 'white'}}>Settings Screen</Text>
       </View>
     </SafeAreaView>
@@ -12,6 +21,7 @@ export default function SettingsScreen() {
 }
 
 const s = StyleSheet.create({
+  glow: {...StyleSheet.absoluteFillObject },
   safe: { flex: 1, backgroundColor: "#000" },
   screen: { flex: 1, padding: 16 },
 });
